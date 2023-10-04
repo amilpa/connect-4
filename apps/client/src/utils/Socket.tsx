de5ba5fb2db1d3f0a557e22fc6311ae02c528d60
Amil P A <amilpa2020@gmail.com>
2023-10-04 23:10:44 +0530

    create context

--- apps/client/src/utils/Socket.tsx
+++ apps/client/src/utils/Socket.tsx
@@ -1,4 +1,21 @@
-import { createContext } from "react";
-import { io } from "socket.io-client";
+import { createContext, useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
+import { Outlet } from "react-router-dom";
 
-const socketContext = createContext<SocketIOClient.Socket>();
+type SocketContext = Socket | null;
+
+export const socketContext = createContext<SocketContext>(null);
+
+export function SocketProvider() {
+  const [socket, setSocket] = useState<SocketContext>(null);
+
+  useEffect(() => {
+    setSocket(io(import.meta.env.VITE_SOCKET_URL));
+  }, []);
+
+  return (
+    <socketContext.Provider value={socket}>
+      <Outlet />
+    </socketContext.Provider>
+  );
+}
