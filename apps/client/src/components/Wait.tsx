00854673a261f56b97cc2403ba5b024a8a6213b9
Amil P A <amilpa2020@gmail.com>
2023-10-05 21:56:52 +0530

    sync

--- apps/client/src/components/Wait.tsx
+++ apps/client/src/components/Wait.tsx
@@ -1,16 +1,20 @@
 import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Navigate } from "react-router-dom";
 import { socketContext } from "../utils/Socket";
 
 export default function Wait() {
   const [ready, setReady] = useState(false);
-  const [room, setRoom] = useState("");
+  const [gameCode, setGameCode] = useState("");
+
+  const navigate = useNavigate();
+
   const socket = useContext(socketContext);
+
   useEffect(() => {
     socket?.emit("create");
     socket?.on("joined", (roomName) => {
-      setRoom(roomName);
+      setGameCode(roomName);
       return;
     });
     socket?.on("ready", () => {
@@ -23,11 +27,11 @@ export default function Wait() {
       <div className="absolute left-1/2 top-44 w-max -translate-x-1/2 rounded-lg bg-neutral-900 p-8">
         <h1 className="">Waiting for people to join...</h1>
         <h1 className="mt-4 text-center text-lg sm:text-xl">
-          Room no: <span className="font-semibold text-yellow-400">{room}</span>
+          Room no:{" "}
+          <span className="font-semibold text-yellow-400">{gameCode}</span>
         </h1>
       </div>
     );
   }
-
-  return <Navigate to="/play" />;
+  navigate("/game", { state: { gameCode: gameCode } });
 }
